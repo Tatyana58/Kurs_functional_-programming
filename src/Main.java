032ae7d35cf3f7c0e119016112f17675904f237a
Tatyana58 <tatbir@rambler.ru>
2023-03-05 23:14:26 +0300

    Начало Вводим строку и подсчитываем количество слов Сортировка ключей по убыванию

--- src/Main.java
+++ src/Main.java
@@ -15,35 +15,40 @@ yourapp the quick brown fox jumps over the lazy dog
 import java.awt.*;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.maxBy;
 import static java.util.stream.Collectors.toMap;
 
 public class Main {
+    static Integer count;
     static Scanner in = new Scanner(System.in);
-    static String massage;
     public static void main(String[] args) {
         //String messageWord = "quick brown fox jumps over the lazy dog the the the";
         System.out.print("Введите строку : ");
         String messageWord = in.nextLine();
         String[] words = messageWord.toLowerCase().replace("[-,.;:?!№@%^&*()_=+~` |]","").split("\\s");
 
-        Map<String,Integer> mapWord = new HashMap<>();
+        Map<String,Integer> mapWord = new TreeMap<String,Integer>(Comparator.reverseOrder());
         for (String word : words) {
             if (!word.isEmpty()) {
-                Integer count = mapWord.get(word);
+                count = mapWord.get(word);
                 if (count == null) {
                     count=0;
                 }
                     mapWord.put(word, ++count);
+
             }
         }
+        System.out.println(maxBy(count)-1);
         System.out.println("Всего " + words.length + " слов в тексте");
         for (String word : mapWord.keySet()) {
             System.out.println(mapWord.get(word) + " -  " + (word));
         }
     }
-    public static String input() {
-        massage = String.valueOf(in.hasNextLine());
-        return massage;
+
+    private static int maxBy(Integer count) {
+        count++;
+        return count;
     }
 }
