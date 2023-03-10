e203c516445ce9f54bb4d5d011c4169148750c7e
Tatyana58 <tatbir@rambler.ru>
2023-03-10 17:20:22 +0300

    Начало Вводим строку и подсчитываем количество слов Сортировка ключей по убыванию Отредактировала Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -18,38 +18,33 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.maxBy;
-import static java.util.stream.Collectors.toMap;
-
-public class Main {
+public class Main{
     static Integer count;
     static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        //Строка для ввода :  quick brown fox jumps over the lazy dog the the the
+        //Строка для ввода :  quick brown fox jumps over the lazy dog the the the brown fox jumps over the lazy dog the the the sgf drgdr geert erte rferf
         System.out.print("Введите строку : ");
         String messageWord = in.nextLine();
         String[] words = messageWord.toLowerCase().replace("[-,.;:?!№@%^&*()_=+~` |]","").split("\\s");
 
-        Map<String,Integer> mapWord = new TreeMap<String,Integer>(Comparator.reverseOrder());
+        Map<String,Integer> mapWord = new TreeMap<String,Integer>();//Comparator.reverseOrder()
         for (String word : words) {
-            if (!word.isEmpty()) {
+            if (!word.isEmpty()) { //проверяет, пустая ли строка
                 count = mapWord.get(word);
                 if (count == null) {
                     count=0;
                 }
-                    mapWord.put(word, ++count);
+                    mapWord.put(word, ++count);  //Чтобы добавить элементы, вызываем метод put()
             }
         }
-        System.out.println(maxBy(count)-1);
-        System.out.println("Всего " + words.length + " слов в тексте");
-        for (String word : mapWord.keySet()) {
-            System.out.println(mapWord.get(word) + " -  " + (word));
-        }
 
-    }
+        System.out.println("Всего слов в тексте - " + words.length);
+        System.out.println("Топ - 10 часто используемых слов в данном тексте.");
 
-    private static int maxBy(Integer count) {
-        count++;
-        return count;
+        mapWord.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(System.out::println);
     }
 }
